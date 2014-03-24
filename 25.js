27080f457d9e04f007819aecadebd67758f77a0e
judsonbc <judsonbc@yahoo.com>
2014-03-24 18:56:22 +0800

    new3

--- 25.js
+++ 25.js
@@ -1,1 +1,19 @@
-window.addEventListener('load', function() {new FastClick(document.body)}, false)
+
+Form2.innerHTML=[  "<div id='Image10' ><img src='images/bathing1.jpg'  height=100% width=100% name='Image10_img' style='border:1' usemap='Image10_map'/></div>",
+  "<div id='Image1Copy' ><img src='images/shampoo.jpg'  height=100% width=100% name='Image1Copy_img' style='' usemap='Image1Copy_map'/></div>",
+  "<div id='Image2Copy' ><img src='images/soap.jpg'  height=100% width=100% name='Image2Copy_img' style='' usemap='Image2Copy_map'/></div>",
+  "<div id='Image3Copy' ><img src='images/towel.jpg'  height=100% width=100% name='Image3Copy_img' style='' usemap='Image3Copy_map'/></div>",
+  "<div id='Image4Copy' ><img src='images/nail_tool.jpg'  height=100% width=100% name='Image4Copy_img' style='' usemap='Image4Copy_map'/></div>",
+  "<div id='Image5Copy' ><img src='images/deo_tool.jpg'  height=100% width=100% name='Image5Copy_img' style='' usemap='Image5Copy_map'/></div>",
+  "<div id='Image6Copy' ><img src='images/ear_tool.jpg'  height=100% width=100% name='Image6Copy_img' style='' usemap='Image6Copy_map'/></div>",
+  "<label id='Label3' class='graytitle' style='text-align:center;  font-size:16px; font-family:helvetica; font-style:normal; font-weight:bold; color:black; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >Drag the proper tools here</label>",
+  NSB.NavBar_jqm('TitleBar2', 'Home, About', '12', 'helvetica', 'normal', 'bold', 'c', '', 'none', '0', ' '),
+  "<div id=DragDrop1></div>",
+  "<div id=DragDrop2></div>",
+  "<div id=DragDrop3></div>",
+  "<div id=DragDrop5></div>",
+  "<div id=DragDrop4></div>",
+  "<div id=DragDrop6></div>",
+  "<label id='Label6' class='graytitle' style='text-align:left;  font-size:12px; font-family:helvetica; font-style:normal; font-weight:bold; color:black; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >Hygiene Level</label>",
+  "<input type='range' id='Slider1' class=''  style='color:black; background-color:white; Horizontal ' name='' disabled=\'disabled\'  readonly value='0' min='0' max='150' step='1' data-role='none' >",
+  ].join('');
