c82ae87e3a28dc4de49be0fa38e20f047984dce0
judsonbc <judsonbc@yahoo.com>
2014-03-24 15:28:05 +0800

    new

--- 19.js
+++ 19.js
@@ -1,5 +1,14 @@
 
-Form3.innerHTML=[  NSB.TitleBar("TitleBar3", "", "leftnav", "", "home", "Home,Right", " style= class=default"),
-  "<video id='Video1' style='' src='video\Brushing.3gp'  autoplay  ></video>",
-  "<label id='Label1' class='graytitle' style='text-align:center;  font-size:12px; font-family:arial; font-style:normal; font-weight:normal; color:black; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >Label</label>",
+Form1.innerHTML=[  "<div id='Image1' ><img src='images/bathing1_icon.jpg'  height=100% width=100% name='Image1_img' style='' usemap='Image1_map'/></div>",
+  "<div id='Image2' ><img src='images/brushing_icon.jpg'  height=100% width=100% name='Image2_img' style='' usemap='Image2_map'/></div>",
+  "<div id='Image3' ><img src='images/face.jpg'  height=100% width=100% name='Image3_img' style='' usemap='Image3_map'/></div>",
+  "<div id='Image4' ><img src='images/nail_icon.jpg'  height=100% width=100% name='Image4_img' style='' usemap='Image4_map'/></div>",
+  "<div id='Image5' ><img src='images/washing_icon.jpg'  height=100% width=100% name='Image5_img' style='' usemap='Image5_map'/></div>",
+  "<div id='Image6' ><img src='images/ear_icon.jpg'  height=100% width=100% name='Image6_img' style='' usemap='Image6_map'/></div>",
+  "<div id='Image7' ><img src='images/deo_icon.jpg'  height=100% width=100% name='Image7_img' style='' usemap='Image7_map'/></div>",
+  "<div id='Image8' ><img src='images/navel_icon.jpg'  height=100% width=100% name='Image8_img' style='' usemap='Image8_map'/></div>",
+  "<div id='Image9' ><img src='images/hair_icon.jpg'  height=100% width=100% name='Image9_img' style='' usemap='Image9_map'/></div>",
+  "<label id='Label2' class='graytitle' style='text-align:center;  font-size:16px; font-family:helvetica; font-style:normal; font-weight:bold; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >Select Task</label>",
+  NSB.NavBar_jqm('NavBar2', 'Home, About', '12', 'helvetica', 'normal', 'bold', 'c', '', 'none', '0', ' '),
+  "<label id='Label5' class='graytitle' style='text-align:center;  font-size:18px; font-family:arial; font-style:normal; font-weight:normal; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >Proper Human Hygiene</label>",
   ].join('');
