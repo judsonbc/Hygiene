1fcf43287dcab0387da65e90b016d001c52c67c0
judsonbc <judsonbc@yahoo.com>
2015-02-13 07:52:02 +0800

    a

--- 25.js
+++ 25.js
@@ -5,9 +5,8 @@ Form1.innerHTML=[  "<div id='Image1' ><img src='images/bathing1_icon.jpg'  heigh
   "<div id='Image4' ><img src='images/nail_icon.jpg'  height=100% width=100% name='Image4_img' style='' usemap='Image4_map'/></div>",
   "<div id='Image5' ><img src='images/washing_icon.jpg'  height=100% width=100% name='Image5_img' style='' usemap='Image5_map'/></div>",
   "<div id='Image6' ><img src='images/ear_icon.jpg'  height=100% width=100% name='Image6_img' style='' usemap='Image6_map'/></div>",
-  "<div id='Image7' ><img src='images/deo_icon.jpg'  height=100% width=100% name='Image7_img' style='' usemap='Image7_map'/></div>",
-  "<div id='Image8' ><img src='images/navel_icon.jpg'  height=100% width=100% name='Image8_img' style='' usemap='Image8_map'/></div>",
-  "<div id='Image9' ><img src='images/hair_icon.jpg'  height=100% width=100% name='Image9_img' style='' usemap='Image9_map'/></div>",
+  "<div id='Image7' ><img src='images/navel_icon.jpg'  height=100% width=100% name='Image7_img' style='' usemap='Image7_map'/></div>",
+  "<div id='Image8' ><img src='images/deo_icon.jpg'  height=100% width=100% name='Image8_img' style='' usemap='Image8_map'/></div>",
   "<label id='Label5' class='graytitle' style='text-align:center;  font-size:25px; font-family:arial; font-style:normal; font-weight:bold; color:green; background-color:white; border-style:solid;border-color:white;border-width:1px; ' >Personal Hygiene</label>",
   "<canvas id='PictureBox2Copy' height='56' width='72' name='PictureBox2Copy_img'  style='background-color:white; border-style:none; '></canvas>",
   "<canvas id='PictureBox1Copy' height='56' width='152' name='PictureBox1Copy_img'  style='background-color:white; border-style:none; '></canvas>",
@@ -17,4 +16,5 @@ Form1.innerHTML=[  "<div id='Image1' ><img src='images/bathing1_icon.jpg'  heigh
   "<label id='Moves1' class='graytitle' style='text-align:center;  font-size:30px; font-family:helvetica; font-style:normal; font-weight:bold; color:green; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >15</label>",
   "<label id='Label9' class='graytitle' style='text-align:center;  font-size:12px; font-family:helvetica; font-style:normal; font-weight:bold; color:red; background-color:white; border-style:;border-color:transparent;border-width:1px; ' >Select Task</label>",
   NSB.TitleBar("TitleBar4", "Main", "leftnav", "rightnav", "setup", "info", " style= class=transparent"),
+  "<div id='Image9' ><img src='' display:none; height=100% width=100% name='Image9_img' style='display:none;' usemap='Image9_map'/></div>",
   ].join('');
