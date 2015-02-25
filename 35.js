46cad310e535e1f96cac6d72efc5f58db6f2b709
judsonbc <judsonbc@yahoo.com>
2015-02-25 18:48:41 +0800

    i

--- 35.js
+++ 35.js
@@ -1,4 +1,4 @@
 
 Form6.innerHTML=[  NSB.TitleBar("TitleBar7", "Perfect!!!", "leftnav", "rightnav", "home", "", " style= class=transparent"),
-  "<video id='Video1' style='' src=''   controls ></video>",
+  "<video id='Video1' style='' src=''  autoplay controls ></video>",
   ].join('');
